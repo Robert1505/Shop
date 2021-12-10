c6441dea6fb583800f8cd0deb1058a8e155792c3
Patainea Robert <patainearobert@yahoo.com>
2021-12-10 16:23:31 +0200

    added list of products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import ProductCard from "./Card";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Grid container spacing={5} sx = {{padding: "30px"}}>
+        <Grid item xs={3}>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ProductCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/Card.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function ProductCard() {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="210"
+        image="https://picsum.photos/536/354"
+        alt="random pic"
+      />
+      <CardContent
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "16px 8px 0px 12px"
+        }}
+      >
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          fontFamily="Poppins"
+          fontWeight="700"
+          sx={{ margin: 0 }}
+        >
+          iPhone 13
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          fontFamily="Poppins"
+          fontWeight="400"
+        >
+          10$
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Favourite</Button>
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+  );
+}
