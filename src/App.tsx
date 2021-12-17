383723292e1959a8e0263a4e2fe6809062de94ed
Patainea Robert <patainearobert@yahoo.com>
2021-12-17 16:34:47 +0200

    search progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import ProductCard from "./Card";
@@ -18,19 +18,44 @@ function App() {
     { name: "Smart TV", price: 850 },
   ];
 
+  const [filteredProducts, setFilteredProducts] =
+    useState<ProductInformation[]>(listOfProducts);
+
   const [favouriteProducts, setFavouriteProducts] = useState<
     ProductInformation[]
   >([]);
 
   const [cartProducts, setCartProducts] = useState<ProductInformation[]>([]);
 
+  const [keyword, setKeyword] = React.useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  useEffect(() => {
+    if (keyword === "") {
+      setFilteredProducts(listOfProducts);
+      return;
+    }
+    // Only the products with the name equal to the keyword will remain in the filtered list
+    const results = listOfProducts.filter(
+      (product: ProductInformation) => product.name === keyword
+    );
+    setFilteredProducts(results);
+  }, [keyword]);
+
   return (
     <div className="App">
-      <Header favouriteBadgeCount={favouriteProducts.length} cartBadgeCount={cartProducts.length} />
-      <Input placeholder="Search" />
+      <Header
+        favouriteBadgeCount={favouriteProducts.length}
+        cartBadgeCount={cartProducts.length}
+      />
+      <div style={{ marginTop: 25 }}>
+        <Input placeholder="Search" value={keyword} onChange={handleChange} />
+      </div>
       <Grid container spacing={5} sx={{ padding: "30px" }}>
-        {listOfProducts.map((product: ProductInformation) => (
-          <Grid item xs={3}>
+        {filteredProducts.map((product: ProductInformation) => (
+          <Grid key={`gridKey-${product.name}`} item xs={3}>
             <ProductCard
               name={product.name}
               price={product.price}
@@ -48,11 +73,10 @@ function App() {
                 );
 
                 // productIndex will be equal to -1 if the product doesn't exist, otherwise it exists
-                if(productIndex === -1){
+                if (productIndex === -1) {
                   copy.push(product);
-                }
-                else{
-                  copy.splice(productIndex,1);
+                } else {
+                  copy.splice(productIndex, 1);
                 }
 
                 setFavouriteProducts(copy);
