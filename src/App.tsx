18671e921ef7c75f1b99ff4c799daba6133ac7a3
Patainea Robert <patainearobert@yahoo.com>
2022-01-07 15:14:22 +0200

    search done

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,17 @@ import { Grid, Input } from "@mui/material";
 export type ProductInformation = {
   name: string;
   price: number;
+  image: string;
 };
 
 function App() {
   const listOfProducts: ProductInformation[] = [
-    { name: "iPhone 13", price: 1000 },
-    { name: "iPhone 12", price: 850 },
-    { name: "iPhone 11", price: 650 },
-    { name: "Pampers", price: 10 },
-    { name: "Smart TV", price: 850 },
+    { name: "iPhone 13", price: 1000, image: "https://lcdn.altex.ro/resize/media/catalog/product/T/e/2bd48d28d1c32adea0e55139a4e6434a/Telefon_APPLE_iPhone_13_Pro_5G_128GB_Graphite_6_.jpg" },
+    { name: "iPhone 12", price: 850, image: "https://s13emagst.akamaized.net/products/33600/33599418/images/res_8cb143da1a4c41323467c164fbb5973f.jpg" },
+    { name: "iPhone 11", price: 650, image: "https://lcdn.altex.ro/resize/media/catalog/product/i/P/2bd48d28d1c32adea0e55139a4e6434a/iPhone_11_Red_2-up_Vertical_US-EN_SCREEN_d64e5a73.jpg"  },
+    { name: "Pampers", price: 10, image: "https://www.agmarket.ro/image/catalog/DEO/PAMPERS%203%2090%201.jpg"},
+    { name: "Smart TV", price: 850, image: "https://images.samsung.com/is/image/samsung/p6pim/ro/ue32t5372cuxxh/gallery/ro-full-hd-tv-ue32t5372cuxxh-front-black-440358521?$720_576_PNG$"  },
+    { name: "Samsung S20", price: 550, image: "https://www.mytrendyphone.ro/images/Samsung-Galaxy-S20-FE-Duos-128GB-Cloud-Navy-8806090716935-02102020-01-p.jpg"  },
   ];
 
   const [filteredProducts, setFilteredProducts] =
@@ -39,7 +41,7 @@ function App() {
     }
     // Only the products with the name equal to the keyword will remain in the filtered list
     const results = listOfProducts.filter(
-      (product: ProductInformation) => product.name === keyword
+      (product: ProductInformation) => product.name.toLowerCase().includes(keyword.toLowerCase())
     );
     setFilteredProducts(results);
   }, [keyword]);
@@ -51,7 +53,7 @@ function App() {
         cartBadgeCount={cartProducts.length}
       />
       <div style={{ marginTop: 25 }}>
-        <Input placeholder="Search" value={keyword} onChange={handleChange} />
+        <Input placeholder="Search" value={keyword} onChange={handleChange} type = "search"/>
       </div>
       <Grid container spacing={5} sx={{ padding: "30px" }}>
         {filteredProducts.map((product: ProductInformation) => (
@@ -59,6 +61,7 @@ function App() {
             <ProductCard
               name={product.name}
               price={product.price}
+              image = {product.image}
               onAddToCartClick={() => {
                 let copy = [...cartProducts];
                 copy.push(product);
